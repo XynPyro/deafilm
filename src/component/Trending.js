08bd444cd4e0221b6a4e0fe50d9630fa526c9078
Moshe Dayan <xynpyro@gmail.com>
2023-02-27 20:36:12 +0700

    added react-awesome-reveal and fade animation to trending

--- src/component/Trending.js
+++ src/component/Trending.js
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { Fade } from "react-awesome-reveal";
 
 import duneImage from "../assets/images/trending/dune.jpg";
 import avatarImage from "../assets/images/trending/avatar.jpg";
@@ -10,103 +11,105 @@ import venomImage from "../assets/images/trending/venom.jpg";
 const Trending = () => {
   return (
     <Container>
-      <Row>
-        <p className="title mt-3 text-center pe-none">TRENDING FILMS</p>
-        <Col md={4} className="movieWrapper mt-5">
-          <div className="card bg-transparent movieImage">
-            <img class="card-img" src={duneImage} alt="Dune 2021" />
-            <div className="bg-dark">
-              <div className="p-2 m-1 text-white pe-none">
-                <h5 class="card-title text-center">Dune 2021</h5>
-                <p class="card-text text-left">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content.
-                </p>
-                <p class="card-text text-left">Last updated 3 mins ago</p>
+      <Fade direction="down" duration={500}>
+        <Row>
+          <p className="title mt-3 text-center pe-none">TRENDING FILMS</p>
+          <Col md={4} className="movieWrapper mt-5">
+            <div className="card bg-transparent movieImage">
+              <img class="card-img" src={duneImage} alt="Dune 2021" />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white pe-none">
+                  <h5 class="card-title text-center">Dune 2021</h5>
+                  <p class="card-text text-left">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content.
+                  </p>
+                  <p class="card-text text-left">Last updated 3 mins ago</p>
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
-        <Col md={4} className="movieWrapper mt-5">
-          <div className="card bg-transparent movieImage">
-            <img class="card-img" src={avatarImage} alt="Dune 2021" />
-            <div className="bg-dark">
-              <div className="p-2 m-1 text-white pe-none">
-                <h5 class="card-title text-center">Avatar</h5>
-                <p class="card-text text-left">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content.
-                </p>
-                <p class="card-text text-left">Last updated 3 mins ago</p>
+          </Col>
+          <Col md={4} className="movieWrapper mt-5">
+            <div className="card bg-transparent movieImage">
+              <img class="card-img" src={avatarImage} alt="Dune 2021" />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white pe-none">
+                  <h5 class="card-title text-center">Avatar</h5>
+                  <p class="card-text text-left">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content.
+                  </p>
+                  <p class="card-text text-left">Last updated 3 mins ago</p>
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
-        <Col md={4} className="movieWrapper mt-5">
-          <div className="card bg-transparent movieImage">
-            <img class="card-img" src={pikachuImage} alt="Dune 2021" />
-            <div className="bg-dark">
-              <div className="p-2 m-1 text-white pe-none">
-                <h5 class="card-title text-center">
-                  Pokémon Detective Pikachu
-                </h5>
-                <p class="card-text text-left">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content.
-                </p>
-                <p class="card-text text-left">Last updated 3 mins ago</p>
+          </Col>
+          <Col md={4} className="movieWrapper mt-5">
+            <div className="card bg-transparent movieImage">
+              <img class="card-img" src={pikachuImage} alt="Dune 2021" />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white pe-none">
+                  <h5 class="card-title text-center">
+                    Pokémon Detective Pikachu
+                  </h5>
+                  <p class="card-text text-left">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content.
+                  </p>
+                  <p class="card-text text-left">Last updated 3 mins ago</p>
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
-        <Col md={4} className="movieWrapper mt-5">
-          <div className="card bg-transparent movieImage">
-            <img class="card-img" src={starwarsImage} alt="Dune 2021" />
-            <div className="bg-dark">
-              <div className="p-2 m-1 text-white pe-none">
-                <h5 class="card-title text-center">
-                  Star Wars Jedi: Fallen Order
-                </h5>
-                <p class="card-text text-left">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content.
-                </p>
-                <p class="card-text text-left">Last updated 3 mins ago</p>
+          </Col>
+          <Col md={4} className="movieWrapper mt-5">
+            <div className="card bg-transparent movieImage">
+              <img class="card-img" src={starwarsImage} alt="Dune 2021" />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white pe-none">
+                  <h5 class="card-title text-center">
+                    Star Wars Jedi: Fallen Order
+                  </h5>
+                  <p class="card-text text-left">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content.
+                  </p>
+                  <p class="card-text text-left">Last updated 3 mins ago</p>
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
-        <Col md={4} className="movieWrapper mt-5">
-          <div className="card bg-transparent movieImage">
-            <img class="card-img" src={thorImage} alt="Dune 2021" />
-            <div className="bg-dark">
-              <div className="p-2 m-1 text-white pe-none">
-                <h5 class="card-title text-center">Thor</h5>
-                <p class="card-text text-left">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content.
-                </p>
-                <p class="card-text text-left">Last updated 3 mins ago</p>
+          </Col>
+          <Col md={4} className="movieWrapper mt-5">
+            <div className="card bg-transparent movieImage">
+              <img class="card-img" src={thorImage} alt="Dune 2021" />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white pe-none">
+                  <h5 class="card-title text-center">Thor</h5>
+                  <p class="card-text text-left">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content.
+                  </p>
+                  <p class="card-text text-left">Last updated 3 mins ago</p>
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
-        <Col md={4} className="movieWrapper mt-5 mb-5">
-          <div className="card bg-transparent movieImage">
-            <img class="card-img" src={venomImage} alt="Dune 2021" />
-            <div className="bg-dark">
-              <div className="p-2 m-1 text-white pe-none">
-                <h5 class="card-title text-center">Venom</h5>
-                <p class="card-text text-left">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content.
-                </p>
-                <p class="card-text text-left">Last updated 4 mins ago</p>
+          </Col>
+          <Col md={4} className="movieWrapper mt-5 mb-5">
+            <div className="card bg-transparent movieImage">
+              <img class="card-img" src={venomImage} alt="Dune 2021" />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white pe-none">
+                  <h5 class="card-title text-center">Venom</h5>
+                  <p class="card-text text-left">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content.
+                  </p>
+                  <p class="card-text text-left">Last updated 4 mins ago</p>
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </Fade>
     </Container>
   );
 };
